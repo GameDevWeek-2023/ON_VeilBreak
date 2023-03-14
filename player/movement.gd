46d70883e3d7b0101110738dc05a18aedd7f25dc
Jene Litsch <litsch.jene@gmail.com>
2023-03-14 19:33:03 +0100

    Semi functional controls

--- player/movement.gd
+++ player/movement.gd
@@ -1,31 +1,52 @@
 extends Node3D
 
-@onready var parent = get_parent();
+@onready var parent : RigidBody3D = get_parent();
 @onready var mesh = get_parent().get_node("pivot")
 
+@export var mouse_sensitivity := 0.2
 @export var acceleration = 5;
 
-@onready var camara: SpringArm3D = get_parent().get_node("player_camara");
+#@onready var camara : Node3D = get_parent().get_node("player_camara");
 
 var target_velocity = Vector3.ZERO
 
-func _physics_process(delta):
-	var move_direction = Vector3.ZERO;
+var _yaw_direction : float = 0.0;
+var _pitch_direction : float = 0.0;
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		self._pitch_direction = -event.relative.y; 
+		self._yaw_direction = -event.relative.x;
+
+
+
+func _physics_process(delta : float):
 	
+	var move_direction = Vector3.ZERO;
 	move_direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left");
 	move_direction.z = Input.get_action_strength("move_back") - Input.get_action_strength("move_forward");
-	move_direction = move_direction.rotated(Vector3.UP, camara.rotation.y).normalized();
+#	move_direction = move_direction.rotated(Vector3.UP, camara.rotation.y).normalized();
 	if move_direction != Vector3.ZERO:
 		move_direction = move_direction.normalized();
 		
+	print(move_direction)
+	parent.apply_force(move_direction * acceleration);
+	
+	var roll_direction = Input.get_action_strength("roll_left") - Input.get_action_strength("roll_right");
+	
+	parent.apply_torque(Vector3.BACK  * roll_direction * 20 * delta);
+	parent.apply_torque(Vector3.RIGHT * _pitch_direction * delta);
+	parent.apply_torque(Vector3.UP    * _yaw_direction   * delta);
+		
 	
 	
+#	parent.move_and_slide();
+#
+#	target_velocity.x += move_direction.x * acceleration;
+#	target_velocity.z += move_direction.z * acceleration;
 	
-	target_velocity.x += move_direction.x * acceleration;
-	target_velocity.z += move_direction.z * acceleration;
-	
-	parent.velocity = target_velocity;
-	parent.move_and_slide();
+#	parent.velocity = target_velocity;
 
 # Called when the node enters the scene tree for the first time.
 #func _ready():
@@ -34,4 +55,5 @@ func _physics_process(delta):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	camara.position = parent.position
+	pass
+#	camara.position = parent.position
--- player/player_camara.gd
+++ player/player_camara.gd
@@ -1,21 +1,20 @@
-extends SpringArm3D
+extends Node3D
 
-@export var mouse_sensitivity := 0.05
-@onready var parent : CharacterBody3D = get_parent();
+@onready var parent : RigidBody3D = get_parent();
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_as_top_level(true);
+#	set_as_top_level(true);
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED);
 	
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		parent.look_at(parent.position, Vector3.UP);
-		rotation_degrees.x -= event.relative.y * mouse_sensitivity;
-#		rotation_degrees.x = clamp(rotation_degrees.x, -90.0, 30.0);
-
-		rotation_degrees.y -= event.relative.x * mouse_sensitivity;
-#		rotation_degrees.y = clamp(rotation_degrees.y, -90.0, 30.0);
+#func _unhandled_input(event: InputEvent) -> void:
+#	if event is InputEventMouseMotion:
+#		parent.look_at(parent.position, Vector3.UP);
+#		rotation_degrees.x -= event.relative.y * mouse_sensitivity;
+##		rotation_degrees.x = clamp(rotation_degrees.x, -90.0, 30.0);
+#
+#		rotation_degrees.y -= event.relative.x * mouse_sensitivity;
+##		rotation_degrees.y = clamp(rotation_degrees.y, -90.0, 30.0);
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
