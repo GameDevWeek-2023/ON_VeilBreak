66016af77c21e93f6b2796745bef70424a884da7
Jene Litsch <litsch.jene@gmail.com>
2023-03-14 11:39:03 +0100

    Basic weapon recoil

--- weapon/Weapon.gd
+++ weapon/Weapon.gd
@@ -1,28 +1,22 @@
 extends Node3D
 
 # Type of ammo to use
-@export
-var projectile : PackedScene
-
-
+@export var projectile : PackedScene;
 
 # Parent Node where projectiles are put in
 # Might refactor later
-@export
-var projectile_manager : Node
+@export var projectile_manager : Node;
 
+@export var cooldown : float = 1.0;
 
+@export var recoil_push : float = 1.0;
+@export var recoil_rotate : float = 1.0;
+
+@export var recoil_curve : Curve;
 
 # Front of barrel wher projectiles are created at
-@onready 
-var muzzle := $Muzzle
-
-
-
-@export 
-var cooldown : float = 1.0
-
-
+@onready var muzzle := $RecoilPivot/Muzzle
+@onready var pivot := $RecoilPivot
 
 var timer : float = 0.0
 
@@ -35,10 +29,15 @@ func shoot():
 		proj.rotation = muzzle.global_rotation;
 		projectile_manager.add_child(proj);
 		print("Shoot");
-		self.timer = self.cooldown
+		self.timer = self.cooldown;
 	
 	
 
 func _process(delta : float):
-	self.timer -= delta;
-		
+	if(self.timer > 0):
+		self.timer -= delta;
+	else:
+		timer = 0.0;
+	var t = (1.0 - self.timer) / self.cooldown
+	self.pivot.position = Vector3.BACK * recoil_push * self.recoil_curve.sample(t);
+	self.pivot.rotation = Vector3.RIGHT * recoil_rotate * self.recoil_curve.sample(t);
