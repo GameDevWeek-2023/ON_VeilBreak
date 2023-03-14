9bdae72aa3dc504df31b9f1fed18eb70047aeba2
Relm <Relmnight@pm.me>
2023-03-14 17:21:26 +0100

    character look at

--- player/player_camara.gd
+++ player/player_camara.gd
@@ -1,6 +1,7 @@
 extends SpringArm3D
 
 @export var mouse_sensitivity := 0.05
+@onready var parent : CharacterBody3D = get_parent();
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,6 +10,7 @@ func _ready():
 	
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
+		parent.look_at(parent.position, Vector3.UP);
 		rotation_degrees.x -= event.relative.y * mouse_sensitivity;
 #		rotation_degrees.x = clamp(rotation_degrees.x, -90.0, 30.0);
 
