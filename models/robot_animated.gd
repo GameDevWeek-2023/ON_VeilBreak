45b5d353797c60209efe3429f94b38cae6bd5825
Jene Litsch <litsch.jene@gmail.com>
2023-03-15 18:39:58 +0100

    Robot skelleton animation

--- models/robot_animated.gd
+++ models/robot_animated.gd
@@ -1,6 +1,25 @@
 extends Node3D
 
+@export var animation_blend_speed = 10.0
+
+var _current_point : Vector2 = Vector2.ZERO;
+var _target_point : Vector2 = Vector2.ZERO;
+
+
+
+func _ready():
+	$AnimationTree.active = true
+
 
 func update_direction(direction : Vector3):
-	$body.rotation = Vector3(direction.x, 0, -direction.z) / 2.0;
-	$body/hips.rotation = Vector3(direction.x, 0, -direction.z)  / 2.0;
+	self._target_point = Vector2(direction.z, -direction.x);
+
+
+
+func _process(delta : float):
+	var delta_direction = (self._target_point - self._current_point);
+	self._current_point += delta_direction * delta * animation_blend_speed;
+	$AnimationTree.set("parameters/Fly/blend_position", self._current_point);
+
+
+
