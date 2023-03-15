69fb8f87736aa84149bc801df310eb743bab2165
Jene Litsch <litsch.jene@gmail.com>
2023-03-15 12:46:09 +0100

    Magic turret shooting at player

--- monster/magic_turret.gd
+++ monster/magic_turret.gd
@@ -2,6 +2,9 @@ extends  "res://Shooter.gd"
 
 @export var team : Team.Enum = Team.Enum.NEUTRAL;
 
+@onready var player : Node3D = get_parent().get_node("Player");
+
 func _process(delta : float):
+	self.look_at(player.position);
 	self._do_shoot();
 	super._process(delta)
