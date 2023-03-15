7ae80c56fd0c19603d8174616fa1cf3adc0cb72d
Jene Litsch <litsch.jene@gmail.com>
2023-03-15 22:06:27 +0100

    Health now calls on_death()

--- objects/components/health.gd
+++ objects/components/health.gd
@@ -6,7 +6,29 @@ extends Node
 
 @onready var parent : Node = get_parent();
 
+
+
 func on_shot(projectile : Node):
 	self.current -= projectile.damage;
 	if(self.current <= 0):
-		parent.queue_free();
+		self._die()
+
+
+
+func _die():
+	self._delegate_to_parent()
+	self._delegate_to_siblings()
+	parent.queue_free();
+	
+
+
+func _delegate_to_parent():
+	if(parent.has_method("on_death")):
+		parent.on_death();
+
+
+
+func _delegate_to_siblings():
+	for sibling in parent.get_children():
+		if(sibling.has_method("on_death")):
+			sibling.on_death()
