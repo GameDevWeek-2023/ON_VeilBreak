fea2a839492b5053a20470281482385c0cbf5cf3
Jene Litsch <litsch.jene@gmail.com>
2023-03-16 14:59:31 +0100

    Fixed gravity hole and added event horizon

--- objects/gravity_hole.gd
+++ objects/gravity_hole.gd
@@ -1,6 +1,14 @@
 extends Node3D
 
 @export var gravity_g : float = 0;
+@export var teleports_to : Node;
 
 func _ready():
-	$Area3D.gravity = 9.81 * gravity_g;
+	$gravity_area.gravity = 9.81 * gravity_g;
+
+
+
+
+func _on_event_horizon_body_entered(body):
+	print("Body fell in")
+
