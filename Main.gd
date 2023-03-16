e4d93645049c8bc7e82e913d6bd6eaac7a859d24
Jene Litsch <litsch.jene@gmail.com>
2023-03-16 02:43:55 +0100

    Scale level

--- Main.gd
+++ Main.gd
@@ -1,21 +1,24 @@
 extends Node3D
 
+
+
 func _ready():
-	self._set_scene(load("res://states/Level.tscn"))
+	self._set_scene(load("res://states/Test.tscn"));
+
 
 
 func _set_scene(scene : PackedScene):
 	var child = scene.instantiate();
 	child.name = "scene";
-	self.add_child(child)
+	self.add_child(child);
 
 
 
 func _remove_scene():
-	$scene.queue_free()
+	$scene.queue_free();
 	
 
 
 func swap_scene(scene : PackedScene):
-	self._remove_scene()
-	self._set_scene(scene)
+	self._remove_scene();
+	self._set_scene(scene);
--- monster/monster_mock_up.gd
+++ monster/monster_mock_up.gd
@@ -1,4 +1,6 @@
 extends RigidBody3D
 
+@export var team : Team.Enum = Team.Enum.NEUTRAL;
+
 func on_death():
 	get_parent().victory()
