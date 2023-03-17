0a6fcbb968f88a376a5b864e256dedbea1d9b36f
Jene Litsch <litsch.jene@gmail.com>
2023-03-17 15:20:18 +0100

    Monster ai can spawn turrets

--- monster/AI.gd
+++ monster/AI.gd
@@ -3,6 +3,7 @@ extends Node
 
 
 
+
 class Idle:
 	var time : float = 0;
 
@@ -60,13 +61,34 @@ class PulseWave:
 
 
 
+
+@export var turret : PackedScene;
+@export var turret_count : int = 1;
+@export var turret_min_distance : float = 50.0
+@export var turret_max_distance : float = 100.0
+
+
+
 @onready var parent : Node3D = self.get_parent();
 @onready var state = Idle.new(5.0);
+@onready var rng = RandomNumberGenerator.new();
 
 
 
 func spawn_turrets():
-	print("SPAWN TURRETS");
+	for x in range(0, self.turret_count):
+		var distance_x = rng.randf_range(self.turret_min_distance, self.turret_max_distance);
+		var distance_y = rng.randf_range(self.turret_min_distance, self.turret_max_distance);
+		var distance_z = rng.randf_range(self.turret_min_distance, self.turret_max_distance);
+		var offset = Vector3(
+			distance_x * (1.0 if (rng.randi() % 2 == 0) else -1.0),
+			distance_y * (1.0 if (rng.randi() % 2 == 0) else -1.0),
+			distance_z * (1.0 if (rng.randi() % 2 == 0) else -1.0),
+		) 
+		var t : Node = turret.instantiate();
+		t.position = parent.position + offset;
+		var level = parent.get_parent();
+		level.add_child(t);
 
 
 func pulse_wave():
