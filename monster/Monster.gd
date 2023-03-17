f90fa93e2307fb9c5b38163aff7814db75634d3b
Jene Litsch <litsch.jene@gmail.com>
2023-03-17 14:03:50 +0100

    Added new monster mock up

---
+++ monster/Monster.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+@export var team : Team.Enum = Team.Enum.NEUTRAL;
+
+func on_death():
+	get_parent().victory();
+
+
+
+func get_health() -> int:
+	return $health.current;
+
+
+
+func get_max_health() -> int:
+	return $health.max;
---
+++ states/Level.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+@onready var main : Node = get_parent();
+
+
+
+
+func _process(delta):
+#	self.gameover()
+	pass
+
+
+
+func gameover():
+	main.swap_scene(load("res://states/GameOver.tscn"))
+
+
+
+func victory():
+	main.swap_scene(load("res://states/Victory.tscn"))
