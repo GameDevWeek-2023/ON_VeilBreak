edc5ac8ba2ba69130e38fea1ded891707909095d
Jene Litsch <litsch.jene@gmail.com>
2023-03-17 16:30:51 +0100

    Pulse wave targets player directly

--- monster/PulseWave.gd
+++ monster/PulseWave.gd
@@ -1,20 +1,28 @@
 extends Node3D
 
-@export var expand_speed : float = 100;
+@export var expand_speed : float = 200;
 @export var damage : int = 1;
+
+@onready var level = get_parent(); 
 
 var team : Team.Enum;
 
-# Called when the node enters the scene tree for the first time.
+
+
 func _ready():
-	pass # Replace with function body.
+	var player = level.get_node("Player");
+	if(player):
+		self.look_at(player.position);
+	else:
+		print("Cannot find player");
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	$anchor.scale.x += delta * expand_speed
 	$anchor.scale.y += delta * expand_speed
 	$anchor.scale.z += delta * expand_speed
+
 
 
 func _on_timer_timeout():
