c81fc1a820b09872259ac58dbc540d6309d1a325
Jene Litsch <litsch.jene@gmail.com>
2023-03-17 15:36:26 +0100

    Improved magic turret attack

--- monster/AI.gd
+++ monster/AI.gd
@@ -40,8 +40,8 @@ class SpawnTurrets:
 		print("AI state: SpawnTurrets");
 		pass
 
-	func start(executor):
-		executor.spawn_turrets()
+	func start(monster):
+		monster.spawn_turrets()
 				
 	func process(delta : float):
 		return Idle.new(5.0)
@@ -53,46 +53,16 @@ class PulseWave:
 		print("AI state: PulseWave");
 		pass
 	
-	func start(executor):
-		executor.pulse_wave()
+	func start(monster):
+		monster.pulse_wave()
 	
 	func process(delta : float):
 		return Idle.new(5.0)
 
 
 
-
-@export var turret : PackedScene;
-@export var turret_count : int = 1;
-@export var turret_min_distance : float = 50.0
-@export var turret_max_distance : float = 100.0
-
-
-
-@onready var parent : Node3D = self.get_parent();
+@onready var monster : Node3D = self.get_parent();
 @onready var state = Idle.new(5.0);
-@onready var rng = RandomNumberGenerator.new();
-
-
-
-func spawn_turrets():
-	for x in range(0, self.turret_count):
-		var distance_x = rng.randf_range(self.turret_min_distance, self.turret_max_distance);
-		var distance_y = rng.randf_range(self.turret_min_distance, self.turret_max_distance);
-		var distance_z = rng.randf_range(self.turret_min_distance, self.turret_max_distance);
-		var offset = Vector3(
-			distance_x * (1.0 if (rng.randi() % 2 == 0) else -1.0),
-			distance_y * (1.0 if (rng.randi() % 2 == 0) else -1.0),
-			distance_z * (1.0 if (rng.randi() % 2 == 0) else -1.0),
-		) 
-		var t : Node = turret.instantiate();
-		t.position = parent.position + offset;
-		var level = parent.get_parent();
-		level.add_child(t);
-
-
-func pulse_wave():
-	print("PULSE WAVE");
 
 
 
@@ -105,4 +75,4 @@ func _process(delta : float):
 	var new_state = self.state.process(delta)
 	if(new_state): 
 		self.state = new_state; 
-		self.state.start(self);
+		self.state.start(monster);
--- monster/Monster.gd
+++ monster/Monster.gd
@@ -1,9 +1,24 @@
 extends Node3D
 
-@export var team : Team.Enum = Team.Enum.NEUTRAL;
+@export var team : Team.Enum = Team.Enum.ENEMY;
+@export var turret : PackedScene;
+@export var turret_count : int = 1;
+@export var turret_min_distance : float = 50.0
+@export var turret_max_distance : float = 100.0
+
+
+
+@onready var rng = RandomNumberGenerator.new();
+@onready var level = get_parent();
+
+
+
+func _ready():
+	rng.randomize();
+
 
 func on_death():
-	get_parent().victory();
+	level.victory();
 
 
 
@@ -14,3 +29,25 @@ func get_health() -> int:
 
 func get_max_health() -> int:
 	return $health.max;
+
+
+
+func spawn_turrets():
+	for x in range(0, self.turret_count):
+		var distance_x = rng.randf_range(self.turret_min_distance, self.turret_max_distance);
+		var distance_y = rng.randf_range(self.turret_min_distance, self.turret_max_distance);
+		var distance_z = rng.randf_range(self.turret_min_distance, self.turret_max_distance);
+		var offset = Vector3(
+			distance_x * (1.0 if (rng.randi() % 2 == 0) else -1.0),
+			distance_y * (1.0 if (rng.randi() % 2 == 0) else -1.0),
+			distance_z * (1.0 if (rng.randi() % 2 == 0) else -1.0),
+		) 
+		var t : Node = turret.instantiate();
+		t.position = position + offset;
+		level.add_child(t);
+
+
+
+
+func pulse_wave():
+	print("PULSE WAVE");
