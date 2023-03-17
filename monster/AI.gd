8ae2fe0a81cf3c46d58943cef1bab396a99fe2a3
Jene Litsch <litsch.jene@gmail.com>
2023-03-17 14:55:37 +0100

    Added simple monster AI

---
+++ monster/AI.gd
@@ -0,0 +1,86 @@
+extends Node
+
+
+
+
+class Idle:
+	var time : float = 0;
+
+	func _init(time : float):
+		print("AI state: Idle");
+		self.time = time;
+
+	func _choose_next():
+		var rng = RandomNumberGenerator.new();
+		rng.randomize();
+		
+		var options := [
+			(func(): return SpawnTurrets.new()),
+			(func(): return PulseWave.new())
+		];
+		
+		var index = rng.randi() % options.size();
+		return options[index].call();
+		
+	func start(executor):
+		pass
+
+	func process(delta : float):
+		self.time -= delta;
+		if(time <= 0):
+			return self._choose_next()
+		else:
+			return null;
+
+
+
+class SpawnTurrets:
+	func _init():
+		print("AI state: SpawnTurrets");
+		pass
+
+	func start(executor):
+		executor.spawn_turrets()
+				
+	func process(delta : float):
+		return Idle.new(5.0)
+		
+		
+		
+class PulseWave:
+	func _init():
+		print("AI state: PulseWave");
+		pass
+	
+	func start(executor):
+		executor.pulse_wave()
+	
+	func process(delta : float):
+		return Idle.new(5.0)
+
+
+
+@onready var parent : Node3D = self.get_parent();
+@onready var state = Idle.new(5.0);
+
+
+
+func spawn_turrets():
+	print("SPAWN TURRETS");
+
+
+func pulse_wave():
+	print("PULSE WAVE");
+
+
+
+func _ready():
+	pass
+
+
+
+func _process(delta : float):
+	var new_state = self.state.process(delta)
+	if(new_state): 
+		self.state = new_state; 
+		self.state.start(self);
